3fad835884a80c7250f8ca68b179df522e407e67
Adolfo Ernesto Custidiano Secchi <cael@Adolfos-MacBook-Pro.local>
2018-08-09 13:17:02 +0100

    adding tests

--- src/test/kotlin/SQLScriptGeneratorTest.kt
+++ src/test/kotlin/SQLScriptGeneratorTest.kt
@@ -1,4 +1,7 @@
+import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.util.Lists
 import org.junit.jupiter.api.Test
 
 internal class SQLScriptGeneratorTest {
@@ -9,13 +12,28 @@ internal class SQLScriptGeneratorTest {
 
     @Test
     fun `should generate scripts`() {
-
-        //given
+        //Given
         val customerEntry = arrayListOf(
                 CustomerEntry("brand1", arrayListOf(Pair("brand2", "info@gmail.com"))),
                 CustomerEntry("brand2", arrayListOf(Pair("brand1", "info1.cael@gmail.com"))))
+
+        //And
+        val expectedFilteredEmails = arrayListOf(
+                CustomerEntry("brand1", arrayListOf(Pair("brand2", "info@gmail.com"))))
+        every { step1(customerEntry) } returns expectedFilteredEmails
+
+        //and
+        val expepectedEntitledEmails = Lists.emptyList<CustomerEntry>()
+        every { step2(expectedFilteredEmails) } returns expepectedEntitledEmails
+
+        //and
+        val expectedScripts = Lists.emptyList<String>()
+        every { step3(expepectedEntitledEmails) } returns expectedScripts
+
         //When
         val result = SQLGenerator(step1, step2, step3).scriptGenerator(customerEntry)
 
+        //Then
+        assertThat(result).isEmpty()
     }
 }

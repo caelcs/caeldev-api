29710401f3656d37e9eec3ea58fe38cd84fe0abf
cael <adolfoecs@gmail.com>
2018-09-25 22:58:40 +0100

    wip

---
+++ src/main/kotlin/uk/co/caeldev/api/Admin.kt
@@ -0,0 +1,13 @@
+package uk.co.caeldev.api
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+
+fun Routing.admin() {
+    get("/health_check", ) {
+        // Check databases/other services.
+        call.respondText("OK")
+    }
+}
--- src/main/kotlin/uk/co/caeldev/api/Application.kt
+++ src/main/kotlin/uk/co/caeldev/api/Application.kt
@@ -1,24 +1,25 @@
 package uk.co.caeldev.api
 
+
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.StatusPages
+import io.ktor.features.*
 import io.ktor.http.HttpStatusCode
+import io.ktor.locations.Locations
 import io.ktor.request.path
 import io.ktor.response.respond
-import io.ktor.response.respondText
-import io.ktor.routing.Routing
-import io.ktor.routing.get
 import io.ktor.routing.routing
 import org.slf4j.event.Level
 
 fun Application.main() {
-    install(StatusPages){
-        exception<Throwable> { _ ->
-            call.respond(HttpStatusCode.InternalServerError)
-        }
+    install(DefaultHeaders)
+    install(ConditionalHeaders)
+    install(Locations)
+    install(Compression)
+
+    install(StatusPages) {
+        exception<NotImplementedError> { call.respond(HttpStatusCode.NotImplemented) }
     }
 
     install(CallLogging) {
@@ -27,18 +28,7 @@ fun Application.main() {
     }
 
     routing {
-        root()
+        admin()
     }
 }
 
-// Extracted route
-fun Routing.root() {
-    get("/") {
-        call.respondText("Hello World!")
-    }
-
-    get("/health_check") {
-        // Check databases/other services.
-        call.respondText("OK")
-    }
-}

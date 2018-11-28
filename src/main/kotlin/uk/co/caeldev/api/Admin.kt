a08de2d5c523cb52275f59f706cca6b7cc82d93c
cael <adolfoecs@gmail.com>
2018-11-28 21:54:48 +0000

    adding metrics endpoint for prometheus

--- src/main/kotlin/uk/co/caeldev/api/Admin.kt
+++ src/main/kotlin/uk/co/caeldev/api/Admin.kt
@@ -4,10 +4,42 @@ import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.koin.dsl.module.module
+import org.koin.ktor.ext.inject
+
+val metricsModule = module {
+    single { MetricRegistry() }
+}
 
 fun Routing.admin() {
+
+    val metricRegistry: MetricRegistry by inject()
+
     get("/health_check") {
-        // Check databases/other services.
         call.respondText("OK")
     }
+
+    get("/metrics") {
+        call.respondText(metricRegistry.getMetrics())
+    }
+}
+
+class MetricRegistry {
+
+    private val registry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    init {
+        ClassLoaderMetrics().bindTo(registry)
+        JvmMemoryMetrics().bindTo(registry)
+        ProcessorMetrics().bindTo(registry)
+        JvmThreadMetrics().bindTo(registry)
+    }
+
+    fun getMetrics(): String = registry.scrape()
 }
--- src/main/kotlin/uk/co/caeldev/api/Application.kt
+++ src/main/kotlin/uk/co/caeldev/api/Application.kt
@@ -6,24 +6,27 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.*
 import io.ktor.http.HttpStatusCode
-import io.ktor.locations.Locations
 import io.ktor.request.path
 import io.ktor.response.respond
 import io.ktor.routing.routing
+import org.koin.standalone.StandAloneContext.startKoin
 import org.slf4j.event.Level
 
 fun Application.main() {
     install(DefaultHeaders)
     install(ConditionalHeaders)
-    install(Locations)
     install(Compression)
+    startKoin(listOf(metricsModule))
 
     install(StatusPages) {
         exception<NotImplementedError> { call.respond(HttpStatusCode.NotImplemented) }
     }
-
+    install(CallId) {
+        generate(10)
+    }
     install(CallLogging) {
-        level = Level.INFO
+        level = Level.TRACE
+        callIdMdc("X-Request-ID")
         filter { call -> call.request.path().startsWith("/") }
     }
 
